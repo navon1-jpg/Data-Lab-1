153de3b1524b8e3e2be32b96561b610abd82ab17
navon1-jpg <navonmoreland1@gmail.com>
2026-02-05 10:25:18 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,23 +1,42 @@
-//
 //  main.cpp
-//  Die.h
+//  Lab 1 Data Strc
 //
 //  Created by Navon M on 1/15/26.
 //
 
-#ifndef Die_h
-#define Die_h
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "Die.h"
 
-class Die {
-private:
-    int sides;
+using namespace std;
 
-public:
-    Die();              // default constructor (6 sides)
-    Die(int s);         // constructor with custom sides
+// Converts numeric score to letter grade
+char getGrade(int score)
+{
+    if (score >= 90)
+        return 'A';
+    else if (score >= 80)
+        return 'B';
+    else if (score >= 70)
+        return 'C';
+    else if (score >= 60)
+        return 'D';
+    else
+        return 'F';
+}
 
-    int roll();         // returns random number 1 to sides
-    int getSides() const;
-};
+int main()
+{
+    // seed random generator
+    srand((unsigned) time(0));
 
-#endif
+    // Die with 100 sides (for scores 1–100)
+    Die scoreDie(100);
+
+    cout << "Testing grades using Die class:\n\n";
+
+    for (int i = 0; i < 10; i++)
+    {
+        int score = scoreDie.roll
+
